d1c47c10d2e52a94e21ade1611e4e9243e8e5c65
Kevin Cruz <kevin_cruzz@outlook.es>
2022-03-07 16:22:10 -0600

    Add change painting color functionality

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 const container = document.querySelector('.container');
+const clear = document.querySelector('.clear-button'); 
+let squareColor = "#000000";
 createDivs();
+initialColor();
+const squares = document.querySelectorAll('.grid-square')
 
 function createDivs() {
     for (let i = 0; i < 256; i++) {
@@ -11,10 +15,26 @@ function createDivs() {
     }
 }
 
-document.body.onmousedown = () => isDown = true;
-document.body.onmouseup = () => isDown = false;
+function initialColor() {
+    const squaresColorPicker = document.querySelector('.paint-color');
+    squaresColorPicker.value = "#000000";
+    squaresColorPicker.addEventListener('input', changeSquareColor);
+    squaresColorPicker.select()
+}
+
+function changeSquareColor(e) {
+    squareColor = e.target.value;
+    squares.forEach((square) => {
+        if (square.style.background) {
+            square.style.background = squareColor;
+        }
+    })
+}
 
 let isDown = false;
+
+document.body.onmousedown = () => isDown = true;
+document.body.onmouseup = () => isDown = false;
 
 container.addEventListener('mousedown', changeColor);
 container.addEventListener('mouseover', changeColor);
@@ -22,6 +42,12 @@ container.addEventListener('mouseover', changeColor);
 function changeColor(e) {
     if (!isDown && e.type === 'mouseover') return;
 
-    e.target.style.background = "black";
+    e.target.style.background = squareColor;
     container.style.background = "white";
 }
+
+clear.addEventListener('click', () => {
+    squares.forEach((square) => {
+        square.style.background = "white";
+    })
+})
