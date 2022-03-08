d6d5f8386742e27f45d60d7d3995c300f45b0ed4
Kevin Cruz <kevin_cruzz@outlook.es>
2022-03-07 20:57:10 -0600

    Add first version of changing number of pixels

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
 const container = document.querySelector('.container');
 const clear = document.querySelector('.clear-button'); 
-let squareColor = "#000000";
+const sizeRange = document.querySelector('.range-sel')
+let squareColor = "#000000", gridColor = "FFFFFF", size = 16;
 createDivs();
 initialColor();
 const squares = document.querySelectorAll('.grid-square')
 
 function createDivs() {
-    for (let i = 0; i < 256; i++) {
+    let width = 38 / size;
+    for (let i = 0; i < (size * size) ; i++) {
         const div = document.createElement('div');
         div.classList.add('grid-square');
         div.classList.add('unselectable')
+        div.style.width = `${width}rem`;
 
         container.appendChild(div);
     }
@@ -17,9 +20,15 @@ function createDivs() {
 
 function initialColor() {
     const squaresColorPicker = document.querySelector('.paint-color');
+    const gridColorPicker = document.querySelector('.grid-color');
+
     squaresColorPicker.value = "#000000";
+    gridColorPicker.value = "#FFFFFF";
+
     squaresColorPicker.addEventListener('input', changeSquareColor);
-    squaresColorPicker.select()
+    gridColorPicker.addEventListener('input', changeGridColor);
+    squaresColorPicker.select();
+    gridColorPicker.select();
 }
 
 function changeSquareColor(e) {
@@ -29,6 +38,11 @@ function changeSquareColor(e) {
             square.style.background = squareColor;
         }
     })
+}
+
+function changeGridColor(e) {
+    gridColor = e.target.value;
+    container.style.background = gridColor;
 }
 
 let isDown = false;
@@ -43,11 +57,43 @@ function changeColor(e) {
     if (!isDown && e.type === 'mouseover') return;
 
     e.target.style.background = squareColor;
-    container.style.background = "white";
+    container.style.background = gridColor;
 }
 
 clear.addEventListener('click', () => {
     squares.forEach((square) => {
-        square.style.background = "white";
+        if (square.style.background){
+            square.style.background = "";
+        }
     })
 })
+
+sizeRange.addEventListener('change', buildNew) 
+
+function buildNew(e) {
+    let newSize = Math.round(e.target.value);
+    let difSizes = newSize - size;
+    if (difSizes < 0) {
+        while (difSizes < 0) {
+            let child = container.lastElementChild;
+            container.removeChild(child);
+            difSizes++;
+        }
+    } else {
+        while (difSizes > 0) {
+            let div = document.createElement('div');
+            div.classList.add('grid-square');
+            div.classList.add('unselectable')
+
+            container.appendChild(div);
+        }
+    }
+    setNewSize(newSize);
+}
+
+function setNewSize(newSize) {
+    newSize = 38 / newSize;
+    squares.forEach((square) => {
+        square.style.width = `${newSize}rem`;
+    });
+}
